1ac1d927593709bf9cb5392696b83f75b8d59023
Reshma28-QA <reshmakuskar2@gmail.com>
2026-03-16 16:57:57 +0530

    Added basic "Hello World" program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/* Program : Hello World
+   Programmer : Reshma Kuskar
+   Description : This program prints 'Hello World' Message
+ */
+
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World !");
+    }
+}
